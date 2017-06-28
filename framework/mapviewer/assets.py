55e538a756e4b6e817adcaf107d17661a3976932
ikselven <ikselven@users.noreply.github.com>
2017-06-28 14:23:39 +0200

    adjustments for arbis due to bundling, improved bundle definition to be more flexible

--- framework/mapviewer/assets.py
+++ framework/mapviewer/assets.py
@@ -13,24 +13,9 @@ def make_bundle(bundle_name, file_list, filters, output):
     bundle = Bundle(*file_list, filters=filters, output=output)
     register(bundle_name, bundle)
 
-# TODO: find a better way to allow globbing in bundles (for ASSETS_DEBUG=True)
-# resolve globs here to not have to mess around with django-assets/webassets
-# see: https://github.com/miracle2k/django-assets/issues/5
-appJs = []
-[appJs.extend(x) for x in [
-    ['js/polyfills.js'],
-    glob_files('js/app/*.js'),
-    glob_files('js/app/core/*.js'),
-    glob_files('js/app/auth/*.js'),
-    glob_files('js/app/csw/*.js'),
-    glob_files('js/app/map/*.js'),
-    glob_files('js/app/swos/*.js'),
-    ['js/dashboard.js']
-]]
-
-make_bundle('appJsBundle', appJs, filters='uglifyjs', output='build/app.bundle.js')
-
-vendorJs = [
+########################################################################################################################
+# Vendor JS
+baseVendorJs = [
     '@turf/turf/turf.min.js',
     'openlayers/dist/ol.js',
     'dygraphs/dist/dygraph.min.js',  # previously: 'lib/dygraph-combined.js', 'lib/dygraph-interaction-api.js'
@@ -50,34 +35,82 @@ vendorJs = [
     'bootstrap-slider/dist/bootstrap-slider.min.js',
     'angular-bootstrap-slider/slider.js',
     'lib/jquery-drags.js',      # not on npmjs.com
-    'chart.js/dist/Chart.min.js',
-    'angular-chart.js/dist/angular-chart.min.js',
-    'd3/d3.min.js',
-    'lib/nv.d3_adjusted_swos.js',   # not on npmjs.com
-    'angular-nvd3/dist/angular-nvd3.min.js',
     'anno.js/dist/anno.js',
     'lib/jquery.fancybox.pack_adjusted.js'  # not on npmjs.com
 ]
 
 # NOTE: we include some unminified libs, so apply basic minification to the whole bundle
-make_bundle('vendorJsBundle', vendorJs, filters='rjsmin', output='build/vendor.bundle.js')
+swosVendorJs = baseVendorJs[:]
+swosVendorJs.extend([
+    'chart.js/dist/Chart.min.js',
+    'angular-chart.js/dist/angular-chart.min.js',
+    'd3/d3.min.js',
+    'lib/nv.d3_adjusted_swos.js',  # not on npmjs.com
+    'angular-nvd3/dist/angular-nvd3.min.js',
+])
+make_bundle('swosVendorJsBundle', swosVendorJs, filters='rjsmin', output='build/swos.vendor.bundle.js')
 
+arbisVendorJs = baseVendorJs[:]
+# TODO: remove once modularity is implemented properly
+arbisVendorJs.extend([
+    'chart.js/dist/Chart.min.js',
+    'angular-chart.js/dist/angular-chart.min.js',
+    'd3/d3.min.js',
+    'lib/nv.d3_adjusted_swos.js',  # not on npmjs.com
+    'angular-nvd3/dist/angular-nvd3.min.js',
+])
+make_bundle('arbisVendorJsBundle', arbisVendorJs, filters='rjsmin', output='build/arbis.vendor.bundle.js')
+
+# NOTE: as Geoss viewer is based on SWOS, include SWOS related bundles beforehand
+geossVendorJs = []
 # NOTE: do not use globbing for this bundle, to prevent including OpenLayers, jQuery and Bootstrap twice
-geossVendorJs = [
+geossVendorJs.extend([
     'lib/geoss/js/jquery-ui.min.js',
     'lib/geoss/js/jquery.dotdotdot.min.js',
     'lib/geoss/js/elasticsearch.jquery.min.js',
     'lib/geoss/js/geossSearchWidget.min.js'
-]
-
+])
 make_bundle('geossVendorJsBundle', geossVendorJs, filters='rjsmin', output='lib/geoss/js/geoss.vendor.bundle.js')
 
+
+########################################################################################################################
+# App JS
+
+# TODO: find a better way to allow globbing in bundles (for ASSETS_DEBUG=True)
+# resolve globs here to not have to mess around with django-assets/webassets
+# see: https://github.com/miracle2k/django-assets/issues/5
+baseAppJs = []
+[baseAppJs.extend(x) for x in [
+    ['js/polyfills.js'],
+    glob_files('js/app/*.js'),
+    glob_files('js/app/core/*.js'),
+    glob_files('js/app/auth/*.js'),
+    glob_files('js/app/csw/*.js'),
+    glob_files('js/app/map/*.js'),
+    ['js/dashboard.js']
+]]
+
+swosAppJs = []
+[swosAppJs.extend(x) for x in [
+    baseAppJs,
+    glob_files('js/app/swos/*.js')
+]]
+make_bundle('swosAppJsBundle', swosAppJs, filters='uglifyjs', output='build/swos.app.bundle.js')
+
+arbisAppJs = []
+[arbisAppJs.extend(x) for x in [
+    baseAppJs
+]]
+make_bundle('arbisAppJsBundle', arbisAppJs, filters='uglifyjs', output='build/arbis.app.bundle.js')
+
+
+########################################################################################################################
+# Vendor CSS
 vendorCss = [
     'bootstrap/dist/css/bootstrap.min.css',
     'css/bootstrap.diff.css',   # changes from modified less files
     'bootstrap-slider/dist/css/bootstrap-slider.min.css',
     'font-awesome/css/font-awesome.min.css',
-    'nvd3/build/nv.d3.min.css',
     'nx-popover/dist/nsPopover.css',
     'fancybox/dist/css/jquery.fancybox.css',
     'openlayers/dist/ol.css',
@@ -85,21 +118,40 @@ vendorCss = [
     'dygraphs/dist/dygraph.min.css'
 ]
 
-make_bundle('vendorCssBundle', vendorCss, filters='cssmin', output='build/vendor.bundle.css')
+swosVendorCss = vendorCss[:]
+swosVendorCss.extend([
+    'nvd3/build/nv.d3.min.css'
+])
+make_bundle('swosVendorCssBundle', swosVendorCss, filters='cssmin', output='build/swos.vendor.bundle.css')
 
-geossVendorCss = [
+arbisVendorCss = vendorCss[:]
+# arbisVendorCss.extend([])
+make_bundle('arbisVendorCssBundle', arbisVendorCss, filters='cssmin', output='build/arbis.vendor.bundle.css')
+
+# NOTE: as Geoss viewer is based on SWOS, include SWOS related bundles beforehand
+geossVendorCss = []
+geossVendorCss.extend([
     'lib/geoss/css/jquery-ui.css',
     'lib/geoss/css/jquery-ui-2.css',
     'lib/geoss/css/custom.min.css',
     'lib/geoss/css/sitecustom.css'
-]
-
+])
 make_bundle('geossVendorCssBundle', geossVendorCss, filters='cssmin', output='lib/geoss/css/geoss.vendor.bundle.css')
 
-appCss = [
+
+########################################################################################################################
+# App CSS
+baseAppCss = [
     'css/dashboard.css',
-    'css/jonas.css',
-    'css/swos.css'
+    'css/jonas.css'
 ]
 
-make_bundle('appCssBundle', appCss, filters='cssmin', output='build/app.bundle.css')
+swosAppCss = baseAppCss[:]
+swosAppCss.extend([
+    'css/swos.css'
+])
+make_bundle('swosAppCssBundle', swosAppCss, filters='cssmin', output='build/swos.app.bundle.css')
+
+arbisAppCss = baseAppCss[:]
+# arbisAppCss.extend([])
+make_bundle('arbisAppCssBundle', arbisAppCss, filters='cssmin', output='build/arbis.app.bundle.css')
