cf5739d84a9ce993c7b56b9ff4e91859ba5ba686
geofranzi <franzi82@gmx.de>
2017-07-02 12:55:07 +0200

    Remove pattern - removed in Django 1.10; adjust changed name

--- framework/webgis/urls.py
+++ framework/webgis/urls.py
@@ -1,33 +1,18 @@
 from django.conf import settings
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
 from webgis import views
 
-import django
-if django.VERSION < (1, 9): #todo remove
-    # register URLs for each app + media URLs
-    urlpatterns = patterns('',
-        url(r'^$', views.index, name='index'),
-        url(r'^session$', views.sessions, name='sessions'),
-        url(r'^authapi/', include('authapi.urls')),
-        url(r'^mapviewer/', include('mapviewer.urls')),
-        url(r'^layers/', include('layers.urls')),
-        url(r'^csw/', include('csw.urls')),
-        url(r'^swos/', include('swos.urls')),
-        url(r'^admin/', include(admin.site.urls)),
-        #url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-else:
-    # register URLs for each app + media URLs
-    urlpatterns = [
-        url(r'^$', views.index, name='index'),
-        url(r'^session$', views.sessions, name='sessions'),
-        url(r'^authapi/', include('authapi.urls')),
-        url(r'^mapviewer/', include('mapviewer.urls')),
-        url(r'^layers/', include('layers.urls')),
-        url(r'^csw/', include('csw.urls')),
-        url(r'^swos/', include('swos.urls')),
-        url(r'^admin/', include(admin.site.urls)),
-        #url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# register URLs for each app + media URLs
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^session$', views.sessions, name='sessions'),
+    url(r'^authapi/', include('authapi.urls')),
+    url(r'^mapviewer/', include('mapviewer.urls')),
+    url(r'^layers/', include('layers.urls')),
+    url(r'^csw/', include('csw.urls')),
+    url(r'^swos/', include('swos.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+    #url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
