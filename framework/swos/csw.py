2190ea9840445f06632e7695d4ce7a5053cde217
geofranzi <franzi82@gmx.de>
2017-09-17 15:15:20 +0200

    rename; check file exists #61

--- framework/swos/csw.py
+++ framework/swos/csw.py
@@ -1,6 +1,8 @@
 from django.template.loader import get_template
 from webgis import settings
 from owslib.util import http_post
+import os.path
+
 from layers.models import MetadataSerializer
 
 
@@ -30,19 +32,20 @@ def create_csw_xml(instance):
     f.write(md_doc)
 
 
-def create_pycsw_entry(id):
+def create_record(id):
     response = http_post(settings.CSW_T_PATH, request=open(settings.MEDIA_ROOT + 'csw/' + str(id) + '_insert.xml').read())
     print response
 
-def delete_pycsw_entry(id):
-    response = http_post(settings.CSW_T_PATH, request=open(settings.MEDIA_ROOT + 'csw/' + str(id) + '_delete.xml').read())
-    print response
+def delete_record(id):
+    if os.path.isfile(settings.MEDIA_ROOT + 'csw/' + str(id) + '_delete.xml'):
+        response = http_post(settings.CSW_T_PATH, request=open(settings.MEDIA_ROOT + 'csw/' + str(id) + '_delete.xml').read())
+        print response
 
 def create_update_csw(instance, action):
-    create_csw_xml(instance)
     if action == "update":
-        delete_pycsw_entry(instance.id)
-    create_pycsw_entry(instance.id)
+        delete_record(instance.id)
+    create_csw_xml(instance)
+    create_record(instance.id)
 
 def delete_csw(instance):
-    delete_pycsw_entry(instance.id)
+    delete_record(instance.id)
