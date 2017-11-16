618cbe4ad1cf6efd83b52c5b8794ae54c218e35b
geofranzi <franzi82@gmx.de>
2017-11-16 11:48:23 +0100

    Add compression (mainly for wetlands.geojson file)

--- framework/webgis/settings.py
+++ framework/webgis/settings.py
@@ -69,6 +69,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.gzip.GZipMiddleware',
     #'authapi.disable.disableCSRF',
 )
 
