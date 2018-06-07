ba12189ae131ec3f413c6924671635bf397ec48b
sibessc <jonas.eberle@gmx.de>
2018-06-07 17:26:13 +0200

    Add class notes also to layer legend

--- framework/static/js/app/swos/swos-layer-controls.directive.js
+++ framework/static/js/app/swos/swos-layer-controls.directive.js
@@ -30,5 +30,18 @@
     function swosLayerControlsCtrl($scope) {
         // necessary so we can use controllerAs syntax
         var slc = this;
+        
+        slc.description = [];
+        slc.description['101'] = [1, 'Vegetated wetland; unknown if natural or artificial wetland'];
+        slc.description['102'] = [2, 'Water bodies; unknown if natural or artificial wetland'];
+        slc.description['103'] = [3, 'Rriver bodies; unknown if natural or artificial wetland'];
+        slc.description['110'] = [4, 'Natural wetland; unknown if vegetated, river or water bodies'];
+        slc.description['111'] = [5, 'Vegetated and natural wetland'];
+        slc.description['112'] = [6, 'Water bodies within a natural wetland'];
+        slc.description['113'] = [7, 'River bodies within a natural wetland'];
+        slc.description['120'] = [8, 'Artificial wetland; unknown if vegetated, river or water bodies'];
+        slc.description['121'] = [9, 'Vegetated and artificial wetland'];
+        slc.description['122'] = [10, 'Water bodies within an artificial wetland'];
+        slc.description['123'] = [11, 'River bodies within an artificial wetland'];
     }
 })();
