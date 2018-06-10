0486297edfffbdfef60ca0c5e9ec4f12cfb044f5
sibessc <jonas.eberle@gmx.de>
2018-06-10 21:11:20 +0200

    Hotfix: Footnotes shall only be shown for indicator layers, not an product layers

--- framework/static/js/app/swos/wetlands-diagram.controller.js
+++ framework/static/js/app/swos/wetlands-diagram.controller.js
@@ -1574,7 +1574,7 @@ console.log(label_list)
                  //   '<tr ng-if="data[0].resol" class="border"><td></td><td>Sensor and spat. resol.</td><td class="legend-label" ng-repeat="val in data[0].resol">{{val[0]}} ({{val[1]}} {{val[2]}})</td></tr>'+
                     '<tr class="border" ng-repeat="item in data | orderBy : \'-key\' ">' +
                     '<td class="legend-color" ng-attr-style="background-color:{{item.color}};">&nbsp;</td>' +
-                    '<td class="legend-label">{{ item.key }}<sup style="padding-left: 5px;" ng-if="active_layer.identifier.includes(\'SWOS_IND\') && diagram.description[item.code]" title="{{diagram.description[item.code][1]}}">{{diagram.description[item.code][0]}}</sup></td>' +
+                    '<td class="legend-label">{{ item.key }}<sup style="padding-left: 5px;" ng-if="legend_type_lulc.includes(\'IND\') && diagram.description[item.code]" title="{{diagram.description[item.code][1]}}">{{diagram.description[item.code][0]}}</sup></td>' +
 
                     '<td ng-if="!diagram.show_percent && !diagram.show_diff" ng-repeat="val in item.values track by $index" class="legend-percent"><span ng-if="val[1] > 0" >{{ diagram.formatValue(val[1].toFixed(2)) }} ha</span><span ng-if="!val[1] > 0" ></span></td>' +
                     '<td ng-if="diagram.show_percent && !diagram.show_diff" ng-repeat="val in item.percent track by $index" class="legend-percent"><span ng-if="val[1] > 0" >{{ diagram.formatValue(val[1].toFixed(2)) }}&nbsp;%</span><span ng-if="!val[1] > 0" ></span><span ng-if="item.percent_total[$index][1] > 0"> ({{ item.percent_total[$index][1].toFixed(2) }}&nbsp;%)</span></td>' +
